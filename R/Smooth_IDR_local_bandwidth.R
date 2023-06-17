f308c625cbf6d7900592037b0595a3aafce0d9c6
timst91 <121156432+timst91@users.noreply.github.com>
2023-06-17 15:25:40 +0200

    Update Smooth_IDR_local_bandwidth.R

--- R/Smooth_IDR_local_bandwidth.R
+++ R/Smooth_IDR_local_bandwidth.R
@@ -121,17 +121,7 @@ smooth_IDR_CDF_h_opt=function(y,x,y_test=y,x_test,c=1,
     if(progress){pb$tick()}
     }
   
-  #if(increasing){
-#    u=smooth_IDR_x_test
-#    for (i in length(u)-0:(length(u)-2)){
-#     if (u[i]<u[i-1]){
-#       if(i>=3)
-#       {u[i-1]=ifelse(u[i-2]<=u[i],mean(u[c(i-2,i)]),u[i])}
-#       else{u[i-1]=u[i]}
-#     }
-#    }
-#    smooth_IDR_x_test=u
-#  }
+  
   if(increasing){
     y_test=sort(y_test)
     u=smooth_IDR_x_test
@@ -275,73 +265,3 @@ smooth_IDR_density_h_opt=function(y,x,x_test,y_test=y,
   
 }
 
-
-# Function that computes OF in terms of c for smooth IDR with optimal local bandwidth selection procedure
-
-OF_c=function(y,x,c,nu,nu_init=2.5,h_init=(log(length(unique(x)))/length(unique(x)))^(1/10),progress=FALSE){
-  sort_ind= match(sort(unique(x)), x)
-  n=length(sort_ind)
-  x=x[sort_ind]
-  y=y[sort_ind]
-  idr_fit=idr(y,data.frame(x))
-  if(progress){pb <- progress_bar$new(total=n)}
-  
-  if(nu!=Inf){
-    K_h=function(u,h){1/h*(1/(nu*pi)^(0.5))*gamma((nu+1)/2)/gamma(nu/2)*(1+(u/h)^2/nu)^(-(nu+1)/2)}
-  }else{
-    K_h=function(u,h){1/h*(2*pi)^(-0.5)*exp(-2*(u/h)^2/2)}
-    }
-  if(nu_init!=Inf){
-    K_h_2=function(u,h){-1/h^3*(nu_init*pi)^(-0.5)*gamma((nu_init+1)/2)/gamma(nu_init/2)*(nu_init+1)/nu_init*(
-    (1+(u/h)^2/nu_init)^(-(nu_init+3)/2)-(nu_init+3)/nu_init*(u/h)^2*(1+(u/h)^2/nu_init)^(-(nu_init+5)/2))}
-  }else{
-      K_h_2=function(u,h){1/h^3*(2*pi)^(-0.5)*exp(-2*(u/h)^2/2)*(u^2/h^2-1)}
-      }
-
- 
-  cdf=idr_fit$cdf
-  count=0
-  logS=0
-  for (i in 1:n){
-    w=c(cdf[i,1])
-    for (j in 2:length(unique(y))){
-      w_j=cdf[i,j]-cdf[i,j-1]
-      w=append(w,w_j)
-    }
-    
-    K2=sapply(sort(unique(y)),function(u){K_h_2(u-y[i],h_init)})
-    
-    second_der= w%*%K2
-    kappa0=K_h(0,1)
-    
-    eps_n=(log(n)/n)^1/3
-    if(nu!=Inf){var_gamma=nu/(nu-2)}else{var_gamma=1}
-    
-    h_opt_Y=c*(4*kappa0*eps_n/(abs(second_der)*var_gamma))^(1/3)
-    
-    K=sapply(sort(unique(y)),function(u){K_h(u-y[i],h_opt_Y)})
-    
-    w_1=cdf[i,c(1,1:(length(unique(y))-1))]
-    w_1[1]=0
-    w_i=cdf[i,]-w_1
-    j_i=which(match(sort(unique(y)), y[i])==1)
-    w_j_i=w_i[j_i]
-    w_i[j_i]=0
-    if(w_j_i!=1){
-      w_i=w_i/(1-w_j_i)
-    }else{
-      count=count+1
-      w_i=w_i}
-    
-   if(sum(w_i%*%K) !=0){
-      logS=logS-1/n*
-        log(w_i%*%K)
-    }else{count=count+1}
-    
-    if(progress){pb$tick()}
-  }
-  #print(logS)
-  return(n/(n-count)*logS)
-  
-  
-}
