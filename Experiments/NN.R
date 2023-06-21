2900e50c31246fea13b190cf54ae9f8a5890ab1d
timst91 <121156432+timst91@users.noreply.github.com>
2023-06-21 14:56:04 +0200

    Update NN.R

--- Experiments/NN.R
+++ Experiments/NN.R
@@ -1,3 +1,4 @@
+
 library(neuralnet)
 library(MASS)
 library(progress)
@@ -13,27 +14,27 @@ target <- normalize(Boston$medv)
 data <- data.frame(target, lapply(Boston[, -14], normalize))
 
 nsplits=20
-mean_test_logS=0
+
 NU=c(2.01,3,4,5,10,20,Inf)
 H=seq(0.01,5,l=10)
 
 grid=expand.grid(NU,H)
+lr=seq(0.01,2,l=10)
 
-pb=progress_bar$new(total=nsplits*nrow(grid))
+#pb=progress_bar$new(total=nsplits*nrow(grid))
+mean_test_logS=0
 
 for (split in 1:nsplits){
+  ind=sample(1:nrow(data))
+  train_ind <- ind[1:floor(0.72 * nrow(data))]
+  val_ind=ind[(floor(0.72 * nrow(data))+1):floor(0.9 * nrow(data))]
+  test_ind=(1:nrow(data))[-c(train_ind,val_ind)]
   
-  train_indices <- sample(1:nrow(data), 0.72 * nrow(data))
-  #train_indices <- sample(1:nrow(data), 0.9 * nrow(data))
+  train_data <- data[train_ind, ]
+  val_data=data[val_ind,]
+  test_data=data[test_ind,]
+ 
   
-  train_data <- data[train_indices, ]
-  
-  rem_data=data[-train_indices, ]
-  val_ind=sample(1:nrow(rem_data), 0.18 * nrow(data))
-  val_data <- rem_data[val_ind, ]
-  test_data=rem_data[-val_ind,]
-  
-  lr=seq(0.01,2,l=10)
   MSE=c()
   nuu=c()
   hh=c()
@@ -51,7 +52,7 @@ for (split in 1:nsplits){
     y=train_data$target
     
     of_h_nu=c()
-    
+    pb$tick()
     
     for (i in 1:nrow(grid)){
       
@@ -61,7 +62,7 @@ for (split in 1:nsplits){
       of_h_nu=append(of_h_nu,
                      of)
       #print(of)
-      pb$tick()
+      #pb$tick()
     }
     
     nuu=append(nuu,as.numeric(grid[which.min(of_h_nu),])[1])
@@ -72,64 +73,36 @@ for (split in 1:nsplits){
   nu_opt=nuu[which.min(MSE)]
   
   
-  train_val_ind=c(train_indices,val_ind)
+  train_val_ind=c(train_ind,val_ind)
   model <- neuralnet(formula, data = data[train_val_ind,], hidden = c(50, 50), linear.output = TRUE, 
-                     act.fct = "logistic", stepmax = 1e6)
+                     act.fct = "logistic", stepmax = 1e6,
+                     learningrate = lr)
   
   
   # Step 7: Make predictions using the trained model
-  predictions <- compute(model, data[-train_val_ind, -1])$net.result
-  
-  predictions <- compute(model, val_data[, -1])$net.result
-  
-  X=compute(model, train_data[, -1])$net.result
-  y=train_data$target
+  X=as.numeric(model$net.result[[1]])
+  y=data[train_val_ind,1] 
+  predictions <- compute(model, data[test_ind, -1])$net.result
   
   
-  
-  val_logS=c()
-  for (i in 1:nrow(grid)){
-    
-    nu=as.numeric(grid[i,])[1]
-    h=as.numeric(grid[i,])[2]
-    pred_idr=c()
-    
-    for(j in 1:nrow(val_data)){
-      pred_idr=append(pred_idr,smooth_IDR_CDF(y,X,h=h,nu=nu,x_test = predictions[j],
-                                              y_test = val_data$target[j]))
-    }
-    
-    val_logS=append(val_logS,-sum(log(pred_idr)))
-    #print(of)
-    pb$tick()
-  }
-  of_h_nu=val_logS
-  nu_opt=as.numeric(grid[which.min(of_h_nu),])[1]
-  h_opt=as.numeric(grid[which.min(of_h_nu),])[2]
-  
-  train_val_ind=c(train_indices,val_ind)
-  model <- neuralnet(formula, data = data[train_val_ind,], hidden = c(50, 50), linear.output = TRUE, 
-                     act.fct = "logistic", stepmax = 1e6)
+  X_test=as.numeric(predictions)
+  y_test=test_data$target
+  test_logS=mean(apply(cbind(X_test,y_test),1, function(u){
+    log(smooth_IDR_density(y,X,h=h_opt,nu=nu_opt,y_test=u[2],x_test=u[1]))}))
   
   
-  # Step 7: Make predictions using the trained model
-  predictions <- compute(model, data[-train_val_ind, -1])$net.result
-  
-  X=compute(model, data[train_val_ind, -1])$net.result
-  y=data[train_val_ind,1]
-  
-  
-  pred_idr=c()
-  
-  for(j in 1:length(predictions)){
-    pred_idr=append(pred_idr,smooth_IDR_CDF(y,X,h=h_opt,nu=nu_opt,x_test = predictions[j],
-                                            y_test = data[-train_val_ind,1][j]))
-  }
-  
-  mean_test_logS=mean_test_logS-1/nsplits*sum(log(pred_idr))
-  
-  print(sum(log(pred_idr)))
+  mean_test_logS=mean_test_logS-1/nsplits*test_logS
+  print(test_logS)
 
   }
 
-pb$terminate()
+#pb$terminate()
+print(mean_test_logS)
+
+
+j=10
+smooth_IDR_density(y,X,h=h_opt,nu=nu_opt,y_test=yy,x_test=X_test[j])
+c(X_test[j],y_test[j])
+
+plot(yy,smooth_IDR_density(y,X,x_test=X_test[j],y_test = yy),type='l')
+abline(v=y_test[j])
