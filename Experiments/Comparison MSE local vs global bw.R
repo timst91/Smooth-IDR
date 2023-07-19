2eeaa0501abe8d4b4a5c0fc8b58aa0307eda47d7
timst91 <121156432+timst91@users.noreply.github.com>
2023-07-19 19:25:34 +0200

    Update Comparison MSE local vs global bw.R

--- Experiments/Comparison MSE local vs global bw.R
+++ Experiments/Comparison MSE local vs global bw.R
@@ -3,86 +3,170 @@ library(devtools)
 #loads the neccessary functions
 source_url("https://raw.githubusercontent.com/timst91/Smooth-IDR/main/R/all.R")
 
+id = as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
 
-
-
-y_test=seq(0,60,l=100)
-
-N=seq(100,5000,l=100)
-
-MSE=c()
-
-MSE_opt=c()
-
-x_test=5
-
-real_cdf=pgamma(y_test,shape=sqrt(x_test),scale=min(max(x_test,2),8))
-
-nsim=3
-
-H=seq(0.1,2,l=10)
-
-Nu=c(seq(2.01,10,l=10),Inf)
-
+H_init=seq(0.01,2,l=5)
 C=c(0.25,0.5,1,2,4)
+NU=c(2.01,3,5,10,Inf)
+H=seq(0.01,4,l=10)
 
-grid=expand.grid(Nu,C)
 
-grid2=expand.grid(Nu,H)
+grid_local=expand.grid(NU,C,H_init)
+grid_global=expand.grid(NU,H)
 
-for (n in N){
-  mse=0
-  mse_opt=0
-  for (j in 1:nsim){
-    y=c()
-    X=sort(runif(n,0,10))
-    
-    for (x in X) {y=append(y,rgamma(1,shape=sqrt(x),scale=min(max(x,2),8)))}
-    
-    of_h_nu=c()
-    
-    for (i in 1:nrow(grid2)){
-      
-      nu=as.numeric(grid2[i,])[1]
-      h=as.numeric(grid2[i,])[2]
-      of=OF_h(y,X,h,nu)
-      of_h_nu=append(of_h_nu,
-                     of)
-    }
-    nu_opt_init=as.numeric(grid2[which.min(of_h_nu),])[1]
-    h_opt_init=as.numeric(grid2[which.min(of_h_nu),])[2]
-    
-    print(c("nu_init=",nu_opt_init,"h_init=",h_opt_init))
-    smooth.idr.cdf=smooth_IDR_CDF(y,X,h=h_opt_init,nu=nu_opt_init, y_test = y_test,x_test=x_test)
-    
-    of_c_nu=c()
-    
-    for(i in 1:nrow(grid)){
-      nu=as.numeric(grid[i,])[1]
-      c=as.numeric(grid[i,])[2]
-      of=OF_c(y,X,c,nu,nu_init = nu_opt_init,h_init = h_opt_init)
-      of_c_nu=append(of_c_nu,of)
-      
-    }
-    nu_opt=as.numeric(grid[which.min(of_c_nu),])[1]
-    c_opt=as.numeric(grid[which.min(of_c_nu),])[2]
-    print(c("nu=",nu_opt,"c=",c_opt))
-    
-    
-    smooth.idr.cdf.h.opt=smooth_IDR_CDF_h_opt(y=y,x=X,c=c_opt, h_init = h_opt_init,
-                                              nu=nu_opt,nu_init = nu_opt_init,
-                                              y_test = y_test,x_test=x_test)
-    mse=mse+1/nsim*mean((smooth.idr.cdf-real_cdf)^2)
-    mse_opt=mse_opt+1/nsim*mean((real_cdf-smooth.idr.cdf.h.opt)^2)
-  }
-  #lines(y_test,smooth.idr.cdf)
-  print(mse)
-  MSE=append(MSE,mse)
-  MSE_opt=append(MSE_opt,mse_opt)
+y_test=seq(0,80,l=50)
+x_test=6
+
+N=seq(50,1000,by=150)
+
+CDFs_local=matrix(ncol=length(N),nrow=length(y_test))
+
+CDFs_global_CV=matrix(ncol=length(N),nrow=length(y_test))
+
+CDFs_global_OF=matrix(ncol=length(N),nrow=length(y_test))
+
+
+for(i in 1:length(N)){
+  n=N[i]
+  X=runif(n,0,10)
+  y=rgamma(n,shape=sqrt(X),scale=min(max(X,2),8))
+  
+  
+  CV_local=apply(grid_local,1,function(w){
+    CV_c(y=y,x=X,c=w[2],nu=w[1],h_init=w[3],progress = 0)
+  })
+ 
+  CV_global=apply(grid_global,1,function(w){
+    CV_h(y=y,x=X,nu=w[1],h=w[2],progress = 0)
+  })
+  
+  OF=apply(grid_global,1,function(w){
+    OF_h(y=y,x=X,nu=w[1],h=w[2],progress = 0)
+  })
+
+  par_local=as.numeric(grid_local[which.min(CV_local),])
+  par_global_cv=as.numeric(grid_global[which.min(CV_global),])
+  par_global_of=as.numeric(grid_global[which.min(OF),])
+  
+  
+  CDFs_local[,i]=smooth_IDR_CDF_h_opt(y,X,x_test = x_test,y_test = y_test,
+                                      c=par_local[2],nu=par_local[1],
+                                      h_init = par_local[3])$cdf
+  
+  CDFs_global_OF[,i]=smooth_IDR_CDF(y,X,x_test = x_test,y_test = y_test,
+                                    h=par_global_of[2],nu=par_global_of[1],
+  )
+  CDFs_global_CV[,i]=smooth_IDR_CDF(y,X,x_test = x_test,y_test = y_test,
+                                    h=par_global_cv[2],nu=par_global_cv[1],
+  )
   
 }
 
-plot(N,MSE,type='l')
-lines(N,MSE_opt,col=2)
-legend("topright",c(expression("global" ~ paste(h[opt])),expression("local" ~paste(h[opt](y)))),col=c(1,2),lty=1)
+
+CDFs=list(CDFs_local=CDFs_local,CDFs_global_OF=CDFs_global_OF,
+          CDFs_global_CV=CDFs_global_CV)
+
+save(
+  list = "CDFs",
+  # save in new file for each id
+  file = paste0("CDFs_MSE", id, ".rda")
+)
+
+
+
+
+#############Evaluation######################
+
+
+
+res <- vector("list", 100) #100 simulations on Euler
+for (id in 1:100) {
+  load(paste0("CDFs_MSE", id, ".rda"))
+  res[[id]] <- CDFs
+}
+res <- do.call(rbind, res)
+
+ids=1:100
+
+CDFs_local=0
+for (i in 1:length(ids)){
+  CDFs_local=CDFs_local+1/length(ids)*res[[i]]
+}
+
+CDFs_of=0
+for (i in (length(ids)+1):(2*length(ids))){
+  CDFs_of=CDFs_of+1/length(ids)*res[[i]]
+}
+
+CDFs_cv=0
+for (i in (2*length(ids)+1):(3*length(ids))){
+  CDFs_cv=CDFs_cv+1/length(ids)*res[[i]]
+}
+
+
+
+y_test=seq(0,80,l=50)
+N=seq(50,1000,by=150)
+
+#ind=1:50  
+
+ind_N=c(1,2,4,5,7)  #less values for N for visibility
+cols=c(2,3,4,5,6)
+#ind_N=1:length(N)
+#cols=2:8
+labels=sapply(ind_N, function(i) substitute(paste("n", " = ", n), list( n= N[i])))
+
+
+
+par(mfrow=c(1,3))
+plot(y_test[ind],pgamma(y_test,shape=sqrt(x_test),scale=min(max(x_test,2),8))[ind],type="l",
+     ylab="",xlab="Threshold")
+
+legend("right",legend=labels,
+       col=cols,
+       lty=1,bty="n",ncol = 1,seg.len = 1,text.width = 5)
+#ncol=2
+
+for(i in 1:length(ind_N)){
+  j=ind_N[i]
+  lines(y_test[ind],CDFs_local[ind,j],col=cols[i],lwd=.5)
+}
+
+plot(y_test[ind],pgamma(y_test,shape=sqrt(x_test),scale=min(max(x_test,2),8))[ind],type="l",
+     ylab="",xlab="Threshold")
+
+
+for(i in 1:length(ind_N)){
+  j=ind_N[i]
+  lines(y_test[ind],CDFs_of[ind,j],col=cols[i])
+}
+
+plot(y_test[ind],pgamma(y_test,shape=sqrt(x_test),scale=min(max(x_test,2),8))[ind],type="l",
+     ylab="",xlab="Threshold")
+
+
+for(i in 1:length(ind_N)){
+  j=ind_N[i]
+  lines(y_test[ind],CDFs_cv[ind,j],col=cols[i])
+}
+
+par(mfrow=c(1,1))
+
+########## MSE plot ###############
+
+MSE_local_N=apply(CDFs_local[,ind_N],2,function(x){
+  sum((x-pgamma(y_test,shape=sqrt(x_test),scale=min(max(x_test,2),8)))^2)})
+
+MSE_of_N=apply(CDFs_of[,ind_N],2,function(x){
+  sum((x-pgamma(y_test,shape=sqrt(x_test),scale=min(max(x_test,2),8)))^2)})
+
+
+MSE_cv_N=apply(CDFs_cv[,ind_N],2,function(x){
+  sum((x-pgamma(y_test,shape=sqrt(x_test),scale=min(max(x_test,2),8)))^2)})
+
+plot(N[ind_N],MSE_local_N,type="b",pch=3,ylim=c(0.3,0.6))
+lines(N[ind_N],MSE_cv_N,type="b",pch=2,col=2)
+
+lines(N[ind_N],MSE_of_N,type="b",pch=4,col=3)
+
